3edd26c93a8c0a3e1c54804895859f496df5029f
Winix <rostamii1378@gmail.com>
2024-10-24 18:16:31 +0330

    add overlay

--- scripts/headerScript.js
+++ scripts/headerScript.js
@@ -1,12 +1,20 @@
-document.getElementById('menu-toggle').addEventListener('click', function() {
+document.getElementById('menu-toggle').addEventListener('click', function(event) {
+    event.stopPropagation(); // جلوگیری از بسته شدن منو هنگام کلیک روی دکمه
     document.querySelector('.side-menu').classList.toggle('open');
+    document.querySelector('.overlay').classList.toggle('active');
   });
- document.addEventListener('click', function(event) {
+  document.querySelector('.overlay').addEventListener('click', function() {
+    document.querySelector('.side-menu').classList.remove('open');
+    document.querySelector('.overlay').classList.remove('active');
+  });
+
+  document.addEventListener('click', function(event) {
     var sideMenu = document.querySelector('.side-menu');
     var burgerMenu = document.querySelector('.burger-menu');
+    var overlay = document.querySelector('.overlay');
   
-    // اگر کلیک خارج از side-menu و burger-menu باشد
     if (!sideMenu.contains(event.target) && !burgerMenu.contains(event.target)) {
-      sideMenu.classList.remove('open'); // بستن منو
+      sideMenu.classList.remove('open');
+      overlay.classList.remove('active');
     }
   });
